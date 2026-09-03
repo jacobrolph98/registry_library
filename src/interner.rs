09e55a4bc6f6e87ea7f0487eb7085eec4c3d9d0c
jacobrolph98 <jacobrolph98@gmail.com>
2026-09-04 00:59:05 +0100

    Created initial repo

---
+++ src/interner.rs
@@ -0,0 +1,50 @@
+use std::{collections::HashMap, marker::PhantomData};
+
+use bevy_ecs::resource::Resource;
+
+pub struct Id<T> {
+    pub index: u32,
+    _marker: PhantomData<fn() -> T>,
+}
+
+impl<T> Clone for Id<T> { fn clone(&self) -> Self { *self } }
+impl<T> Copy for Id<T> {}
+impl<T> PartialEq for Id<T> { fn eq(&self, other: &Self) -> bool { self.index == other.index } }
+impl<T> Eq for Id<T> {}
+impl<T> std::hash::Hash for Id<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) { self.index.hash(state) }
+}
+impl<T> std::fmt::Debug for Id<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Id({})", self.index)
+    }
+}
+
+#[derive(Resource)]
+pub struct Interner<T> {
+    to_id: HashMap<String, Id<T>>,
+    to_string: Vec<String>,
+}
+
+impl <T> Interner<T> {
+    /// Get ID from string if it exists, otherwise register an ID from string
+    pub fn intern(&mut self, s: &str) -> Id<T> {
+        if let Some(&id) = self.to_id.get(s) {
+            return id;
+        }
+        let id = Id { index: self.to_string.len() as u32, _marker: PhantomData };
+        self.to_string.push(s.to_string());
+        self.to_id.insert(s.to_string(), id);
+        id
+    }
+
+    /// Get ID from string if it exists
+    pub fn get_existing(&self, s: &str) -> Option<Id<T>> {
+        self.to_id.get(s).copied()
+    }
+
+    /// Get string from ID if it exists
+    pub fn resolve(&self, id: Id<T>) -> &str {
+        &self.to_string[id.index as usize]
+    }
+}
---
+++ src/registry.rs
@@ -0,0 +1,48 @@
+use bevy_ecs::resource::Resource;
+
+use crate::interner::Id;
+
+pub trait Definition: Send + Sync + Clone + 'static {
+    fn id_str(&self) -> &str;
+}
+
+#[derive(Resource)]
+pub struct Registry<T: Definition> {
+    entries: Vec<Option<T>>
+}
+
+impl<T: Definition> Registry<T> {
+    /// Increase size of vec storing data if necessary
+    fn ensure_capacity(&mut self, id: Id<T>) {
+        let needed = id.index as usize + 1;
+        if self.entries.len() < needed {
+            self.entries.resize_with(needed,  || None);
+        }
+    }
+
+    /// Attempts to register definition by this ID, errors if ID already exists
+    pub fn register(&mut self, id: Id<T>, def: T) -> Result<(), String> {
+        self.ensure_capacity(id);
+        let slot = &mut self.entries[id.index as usize];
+        if slot.is_some() {
+            return Err(format!("Duplicate id: {}", def.id_str()));
+        }
+        *slot = Some(def);
+        Ok(())
+    }
+
+    /// Registers definition by ID, overriding existing data if any
+    pub fn register_or_override(&mut self, id: Id<T>, def: T) {
+        self.ensure_capacity(id);
+        let slot = &mut self.entries[id.index as usize];
+        if slot.is_some() {
+            //warn!("Overriding existing definition: {}", def.id_str());
+        }
+        *slot = Some(def);
+    }
+
+    /// Return definition data corresponding to this ID, or None
+    pub fn get(&self, id: Id<T>) -> Option<&T> {
+        self.entries.get(id.index as usize)?.as_ref()
+    }
+}
