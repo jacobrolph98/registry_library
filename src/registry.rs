bcc6586874534e9b9c879c4036b22c7444603b27
jacobrolph98 <jacobrolph98@gmail.com>
2026-09-04 12:04:39 +0100

    Refactor to remove unnecessary types

--- src/registry.rs
+++ src/registry.rs
@@ -2,16 +2,12 @@ use bevy_ecs::resource::Resource;
 
 use crate::interner::Id;
 
-pub trait Definition: Send + Sync + Clone + 'static {
-    fn id_str(&self) -> &str;
-}
-
 #[derive(Resource)]
-pub struct Registry<T: Definition> {
+pub struct Registry<T> {
     entries: Vec<Option<T>>
 }
 
-impl<T: Definition> Registry<T> {
+impl<T> Registry<T> {
     /// Increase size of vec storing data if necessary
     fn ensure_capacity(&mut self, id: Id<T>) {
         let needed = id.index as usize + 1;
@@ -25,7 +21,7 @@ impl<T: Definition> Registry<T> {
         self.ensure_capacity(id);
         let slot = &mut self.entries[id.index as usize];
         if slot.is_some() {
-            return Err(format!("Duplicate id: {}", def.id_str()));
+            return Err(format!("Duplicate ID registered with {id:?}"));
         }
         *slot = Some(def);
         Ok(())
